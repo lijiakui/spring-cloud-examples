c72a2eccf098dd0699e2025a7268090fe2b0d6b0
lijiakui <jackcoder@126.com>
2018-04-21 16:34:44 +0800

    消息总线

--- spring-cloud-base/spring-cloud-consumer/src/main/java/com/jack/controller/ConsumerController.java
+++ spring-cloud-base/spring-cloud-consumer/src/main/java/com/jack/controller/ConsumerController.java
@@ -1,6 +1,7 @@
 package com.jack.controller;
 
 import com.jack.remote.HelloRemote;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +18,8 @@ public class ConsumerController {
 
     @Resource
     private HelloRemote helloRemote;
-
+    @Value("${config.hello}")
+    private String helloConfig;
     /**
      * say hello
      * @return
@@ -26,5 +28,10 @@ public class ConsumerController {
     public String hello(){
         String message = helloRemote.hello("consumer-server");
         return message;
+    }
+
+    @RequestMapping("readConfig")
+    public String readConfig(){
+        return this.helloConfig;
     }
 }
