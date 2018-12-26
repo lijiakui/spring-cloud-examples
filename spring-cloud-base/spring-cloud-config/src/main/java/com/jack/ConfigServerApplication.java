07f0070b298145d78a7580ceb34863f1e9b11b5a
lijiakui <jackcoder@126.com>
2018-12-26 16:28:39 +0800

    ----

---
+++ spring-cloud-base/spring-cloud-config/src/main/java/com/jack/ConfigServerApplication.java
@@ -0,0 +1,22 @@
+package com.jack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author: LiJiakui
+ * @Description:
+ * @Dat: Create in 下午7:32 18/4/19
+ * @Modifiled By:
+ */
+@SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
